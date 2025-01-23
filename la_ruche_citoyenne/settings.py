bebc03b7e556b4bff0c9fafa5663d49cbfe2eb64
kbakazami <struys.wyvin@gmail.com>
2025-01-23 14:58:03 +0100

    Update database configuration to use MySQL instead of SQLite

--- la_ruche_citoyenne/settings.py
+++ la_ruche_citoyenne/settings.py
@@ -86,8 +86,12 @@ WSGI_APPLICATION = 'la_ruche_citoyenne.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'la_ruche_citoyenne',
+        'USER': 'root',
+        'PASSWORD': '',
+        'HOST':'localhost',
+        'PORT':'3306',
     }
 }
 
